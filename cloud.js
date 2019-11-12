e22ccb7483c5cf8b74decde1ba2c6dd43b9cca23
MURAHASHI Kuriki <mkuriki1990@gmail.com>
2019-11-12 15:56:54 +0900

    first commit

---
+++ cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 2095229,
+    "boundingBox": {
+        "lx": -7.167566,
+        "ly": -4.764079,
+        "lz": -2.052744,
+        "ux": 1.657967,
+        "uy": 4.061453,
+        "uz": 6.772788
+    },
+    "tightBoundingBox": {
+        "lx": -6.926566,
+        "ly": -4.511079,
+        "lz": -1.488149,
+        "ux": -4.391587,
+        "uy": -2.015696,
+        "uz": 4.225022
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 0.068949,
+    "scale": 0.001000,
+    "hierarchyStepSize": 5
+}
